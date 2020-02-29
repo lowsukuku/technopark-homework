27e4b0e3c1931878af2154b8ea96e6c4a883324a
lowsukuku <gordon970530@gmail.com>
2020-02-29 18:51:19 +0300

    added simple stdin reader added debug configuration for windows

--- Source/main.c
+++ Source/main.c
@@ -2,6 +2,15 @@
 
 int main()
 {
-    printf("Hello World!\n");
+    printf("Enter your python code here. Escape character is Ctrl+D for Linux, Ctrl+Z and Enter for Windows\n>> ");
+    int a = 0;
+    while ((a = getchar()) != EOF)
+    {
+        if (a == '\n') 
+        {
+            printf(">> ");
+        }
+    }
+    
     return 0;
 }
